e1228493342181f2b9967616b1926cd590385e6b
Kim Hokyeong <manmen.mi1375@gmail.com>
2025-06-18 10:38:23 +0000

    add submit

--- src/app.tsx
+++ src/app.tsx
@@ -1,43 +1,26 @@
 import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
+import ky from 'ky'
 import './app.css'
 
 export function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState('')
+  const [said, setSaid] = useState('')
+
+
+  async function submit(ev) {
+    ev.preventDefault()
+    const response = await ky(`https://dash.deno.com/playground/cow-say-99?say=${message}`).text()
+    setSaid(response)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p>
-        Check out{' '}
-        <a
-          href="https://preactjs.com/guide/v10/getting-started#create-a-vite-powered-preact-app"
-          target="_blank"
-        >
-          create-preact
-        </a>
-        , the official Preact + Vite starter
-      </p>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
+      <form onSubmit={ev => submit(ev)}>
+        <textarea name="say" value={message} onInput={ev => setMessage(ev.currentTarget.value)}></textarea>
+        <button type="submit">SAY</button>
+        <hr />
+        <textarea name="said" value={said} readonly></textarea>
+      </form>
     </>
   )
 }
